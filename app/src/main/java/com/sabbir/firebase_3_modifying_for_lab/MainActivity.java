9bba7f72e94b25b232fbb2b8cbf9dcbfab334818
Sabbir <jonyhasan01@gmail.com>
2017-09-04 16:48:42 +0600

    Button color and animation

--- app/src/main/java/com/sabbir/firebase_3_modifying_for_lab/MainActivity.java
+++ app/src/main/java/com/sabbir/firebase_3_modifying_for_lab/MainActivity.java
@@ -2,9 +2,17 @@ package com.sabbir.firebase_3_modifying_for_lab;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.R.attr.delay;
 
 /**
  * Created by Sabbir on 015  15 05 17  May.
@@ -19,12 +27,38 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.front_page);
+        //if not given final it won't work inside setOnClickListener
+        final Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
 
-        Button all = (Button) findViewById(R.id.all);
+        final Button all = (Button) findViewById(R.id.all);
+        //all.startAnimation(shake);
         all.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, ForAllStudents.class);
+                view.startAnimation(shake);
+                //setting delay for clicking animation
+                /*all.setEnabled(false);
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        all.setEnabled(true);
+
+                    }
+                }, 10000);*/
+
+                /*all.setEnabled(false);
+
+                Timer buttonTimer = new Timer();
+                buttonTimer.schedule(new TimerTask() {
+
+                    @Override
+                    public void run() {
+                        all.setEnabled(true);
+                    }
+                }, 5000,1000);*/
+
                 startActivity(i);
             }
         });
