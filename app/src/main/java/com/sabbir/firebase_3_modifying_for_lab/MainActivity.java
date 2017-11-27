bcf45272d7bc078af4cc5487047d12bf8411155a
Sabbir <jonyhasan01@gmail.com>
2017-11-27 14:03:10 +0600

    2017_11_17

--- app/src/main/java/com/sabbir/firebase_3_modifying_for_lab/MainActivity.java
+++ app/src/main/java/com/sabbir/firebase_3_modifying_for_lab/MainActivity.java
@@ -1,13 +1,23 @@
 package com.sabbir.firebase_3_modifying_for_lab;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -23,12 +33,89 @@ import static android.R.attr.delay;
 //users can't modify or delete data from app, only insert and see...
 public class MainActivity extends AppCompatActivity {
 
+    private static final int EXTERNAL_STORAGE_PERMISSION_CONSTANT = 100;
+    private static final int REQUEST_PERMISSION_SETTING = 101;
+    private boolean sentToSettings = false;
+    private SharedPreferences permissionStatus;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.front_page);
         //if not given final it won't work inside setOnClickListener
         final Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+
+
+        //
+        // permission
+        //
+        permissionStatus = getSharedPreferences("permissionStatus",MODE_PRIVATE);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this
+                                                , Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                //Show Information about why you need the permission
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Need Storage Permission");
+                builder.setMessage("This app needs storage permission.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        ActivityCompat.requestPermissions(MainActivity.this
+                                                            , new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                                                            , EXTERNAL_STORAGE_PERMISSION_CONSTANT);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else if (permissionStatus.getBoolean(Manifest.permission.WRITE_EXTERNAL_STORAGE,false)) {
+                //Previously Permission Request was cancelled with 'Dont Ask Again',
+                // Redirect to Settings after showing Information about why you need the permission
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Need Storage Permission");
+                builder.setMessage("This app needs storage permission.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        sentToSettings = true;
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                        intent.setData(uri);
+                        startActivityForResult(intent, REQUEST_PERMISSION_SETTING);
+                        Toast.makeText(getBaseContext(), "Go to Permissions to Grant Storage", Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else {
+                //just request the permission
+                ActivityCompat.requestPermissions(MainActivity.this
+                                                    , new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                                                    , EXTERNAL_STORAGE_PERMISSION_CONSTANT);
+            }
+
+
+            SharedPreferences.Editor editor = permissionStatus.edit();
+            editor.putBoolean(Manifest.permission.WRITE_EXTERNAL_STORAGE,true);
+            editor.commit();
+
+        } /*else {
+            //You already have the permission, just go ahead.
+            proceedAfterPermission();
+        }*/
+
+
 
         final Button all = (Button) findViewById(R.id.all);
         //all.startAnimation(shake);
@@ -37,28 +124,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, ForAllStudents.class);
                 view.startAnimation(shake);
-                //setting delay for clicking animation
-                /*all.setEnabled(false);
-                new Handler().postDelayed(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        all.setEnabled(true);
-
-                    }
-                }, 10000);*/
-
-                /*all.setEnabled(false);
-
-                Timer buttonTimer = new Timer();
-                buttonTimer.schedule(new TimerTask() {
-
-                    @Override
-                    public void run() {
-                        all.setEnabled(true);
-                    }
-                }, 5000,1000);*/
-
                 startActivity(i);
             }
         });
